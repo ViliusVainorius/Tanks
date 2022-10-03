3e7f89963415b1758fc818645b3a4d6ee27c8404
velfoxx <viliusvainorius@gmail.com>
2022-10-03 16:09:55 +0300

    gameobject shared class

---
+++ SharedObjects/GameObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedObjects
+{
+    public class GameObject
+    {
+        public int X;
+        public int Y;
+
+        public GameObject()
+        {
+
+        }
+
+        public GameObject(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+
+
+    }
+}
