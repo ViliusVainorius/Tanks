4cac5e9d5af232bf77e0fc73c906b7374b95b8da
vytaute.lipeikaite@gmail.com <vytaute.lipeikaite@gmail.com>
2022-10-03 16:08:17 +0300

    Add gamesession + wall

---
+++ SharedObjects/GameSession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedObjects
+{
+    public class GameSession
+    {
+        public DateTime start_time {get; set;}
+        public DateTime finish_time {get; set;}
+        public int players_count {get; set;}
+        public string difficulty {get; set;}
+        public Wall [] map {get; set;}
+
+
+    }
+}
